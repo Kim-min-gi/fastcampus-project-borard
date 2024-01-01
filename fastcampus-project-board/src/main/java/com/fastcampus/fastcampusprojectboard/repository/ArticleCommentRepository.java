5b18db0dd9379c4dd1bcd0f3bb32c6229cb49812
DANGHYEON <rlaalsrl3023@naver.com>
2024-01-01 16:45:18 +0900

    QueryDsl 지원을 이용하여 api 검색 옵션 추가
    QueryDsl 지원을 이용하여 api 검색 옵션 추가

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.ArticleComment;
+import com.fastcampus.fastcampusprojectboard.domain.QArticle;
+import com.fastcampus.fastcampusprojectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment,Long>
+        , QuerydslPredicateExecutor<ArticleComment>
+        , QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override //스프링 데이터 JPA를 이용해서 인터페이스만 가지고 기능을 다 사용하게끔 접근하고 있어서 디폴트 메소드를 사용하는게 적절
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    };
 }
--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
@@ -1,9 +1,30 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends JpaRepository<Article,Long>
+        , QuerydslPredicateExecutor<Article> // 이 엔티티 안에 있는 모든 필드에 대한 기본 검색 기능을 추가해 준다.
+        , QuerydslBinderCustomizer<QArticle>
+{
+    @Override //스프링 데이터 JPA를 이용해서 인터페이스만 가지고 기능을 다 사용하게끔 접근하고 있어서 디폴트 메소드를 사용하는게 적절
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title,root.content,root.hashtag, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    };
 }
